cf8f82d4565b01b6676f6ad324f154e2ce84291c
Eric <EricPDX@users.noreply.github.com>
2016-11-15 12:26:46 -0800

    Add files via upload
    added main.c

---
+++ T11-firmware/main.c
@@ -0,0 +1,34 @@
+#define F_CPU 16000000UL 
+
+#include <avr/io.h> 
+#include <util/delay.h>
+#include "pwm.h"
+
+void main( void )
+{
+	// Value between 0-255
+	// 0=0v, 255=5v
+	unsigned char DutyCycle = 256/2; 
+
+	// Prescaler value (1,8,64,256,1024) 
+	// Formula for frequency: f = 16000000/(N*510)
+	// Where N = Frequency value
+
+	PWM_On_PD5(DutyCycle);
+	PWM_On_PD6(DutyCycle);	
+	PWM_On_PB3(DutyCycle); 
+
+	unsigned char i=0; 
+	while(1)
+	{
+		for (i = 0; i <= 255; i++)
+		{
+			PWM_Change_PD5(i);
+			PWM_Change_PD6(i);
+			PWM_Change_PB3(i);
+			_delay_ms(100);
+		}
+	}
+}
+
+

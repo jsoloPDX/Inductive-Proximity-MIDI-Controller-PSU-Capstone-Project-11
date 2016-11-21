ea5776c0e7052b648a036a1c3611fa7f6443cd96
EricPDX <EricPDX@users.noreply.github.com>
2016-11-21 11:46:27 -0800

    Cleared main.c of testing code, and setup some of the program skeleton.

--- T11-firmware/main.c
+++ T11-firmware/main.c
@@ -3,32 +3,58 @@
 #include <avr/io.h> 
 #include <util/delay.h>
 #include "pwm.h"
+#include "main.h"
 
 void main( void )
 {
-	// Value between 0-255
-	// 0=0v, 255=5v
-	unsigned char DutyCycle = 256/2; 
+	/*
+	Initialization Stuff should go here. 
+	This can include initial calibration
+	*/ 
+	InitializePorts();
+	AutoCalibration();	
 
-	// Prescaler value (1,8,64,256,1024) 
-	// Formula for frequency: f = 16000000/(N*510)
-	// Where N = Frequency value
 
-	PWM_On_PD5(DutyCycle);
-	PWM_On_PD6(DutyCycle);	
-	PWM_On_PB3(DutyCycle); 
-
-	unsigned char i=0; 
+	/*
+	This is the main polling loop. With the exception of function calls, 
+	the microcontroller should stay here while powered, and after the 
+	initialization. 
+	*/
 	while(1)
 	{
-		for (i = 0; i <= 255; i++)
-		{
-			PWM_Change_PD5(i);
-			PWM_Change_PD6(i);
-			PWM_Change_PB3(i);
-			_delay_ms(100);
+
+		if(IsButtonOnePressed()){
+			//Handle Buttone One
 		}
+
+		if(IsButtonTwoPressed()){
+			//Handle Buttone Two
+		}
+
 	}
 }
 
+
+
+void InitializePorts(void){
+	// Port initialization code here. The function eventually will 
+	// accept inputs/outputs as needed. 
+}
+
+
+void AutoCalibration(void){
+	// Perform auto calibration
+}
+
+
+int IsButtonOnePressed(void){
+	// Return 0 if false, 1 if true
+	return(0);	
+}
+
+
+int IsButtonTwoPressed(void){
+	// Return 0 if false, 1 if true
+	return(0);
+}
 
---
+++ T11-firmware/main.h
@@ -0,0 +1,8 @@
+
+
+// Functions
+void InitializePorts(void);
+void AutoCalibration(void);
+int IsButtonOnePressed(void);
+int IsButtonTwoPressed(void);
+

1a9e034346f3d5a427be34fb25f0a986e9286f5b
EricPDX <EricPDX@users.noreply.github.com>
2016-11-21 20:05:49 -0800

    Added code for button presses, and for one of the LEDs.

--- T11-firmware/main.c
+++ T11-firmware/main.c
@@ -5,6 +5,9 @@
 #include "pwm.h"
 #include "main.h"
 
+#define DelayOn 500
+#define DelayOff 1000
+
 void main( void )
 {
 	/*
@@ -13,6 +16,7 @@ void main( void )
 	*/ 
 	InitializePorts();
 	AutoCalibration();	
+
 
 
 	/*
@@ -24,7 +28,7 @@ void main( void )
 	{
 
 		if(IsButtonOnePressed()){
-			//Handle Buttone One
+			BlinkLED_PD4(3);
 		}
 
 		if(IsButtonTwoPressed()){
@@ -36,10 +40,22 @@ void main( void )
 
 
 
+
+
 void InitializePorts(void){
 	// Port initialization code here. The function eventually will 
 	// accept inputs/outputs as needed. 
+	
+	// Initialize PinD3 as input
+	DDRD &= ~(1<<DDD3);
+
+	// Set PD4 port as output. Initialize as off.
+	DDRD |= (1 << DDD4);
+	PORTD &= ~(1<<DDD4);
 }
+
+
+
 
 
 void AutoCalibration(void){
@@ -47,14 +63,55 @@ void AutoCalibration(void){
 }
 
 
+
+
 int IsButtonOnePressed(void){
+	if(PIND & 0b00100000){
+		// Button pressed. Wait 20 ms, and if still pressed, return.
+		_delay_ms(20);		
+		if(PIND & 0b00100000){
+			// Small delay to handle depress.
+			_delay_ms(100);
+			return(1);
+		}
+	}
 	// Return 0 if false, 1 if true
 	return(0);	
 }
 
 
+
+
 int IsButtonTwoPressed(void){
+	if(PIND & 0b01000000){
+		// Button pressed. Wait 20 ms, and if still pressed, return.
+		_delay_ms(20);		
+		if(PIND & 0b00100000){
+			// Small delay to handle depress.
+			_delay_ms(100);
+			return(1);
+		}
+	}
 	// Return 0 if false, 1 if true
-	return(0);
+	return(0);	
 }
+
+
+
+
+void BlinkLED_PD4(unsigned char NumOfBlinks){
+
+	unsigned char i = 0;
+	for (i = 0; i < NumOfBlinks; i++){ 	
+		// Set PD4 port as on
+		PORTD |= (1 << DDD4);
+		_delay_ms(DelayOn);
+		
+		// Set PD4 port as off
+		PORTD &= ~(1<<DDD4);
+		_delay_ms(DelayOff);
+	}
+}
+
+
 
--- T11-firmware/main.h
+++ T11-firmware/main.h
@@ -5,4 +5,4 @@ void InitializePorts(void);
 void AutoCalibration(void);
 int IsButtonOnePressed(void);
 int IsButtonTwoPressed(void);
-
+void BlinkLED_PD4(unsigned char NumOfBlinks);
